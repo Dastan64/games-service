f2abdfc069e53037ca1304feb492335439dd6862
Dastan <dastan132000@mail.ru>
2022-05-18 16:12:49 +0600

    Добавил компонент прокрутки наверх при переходе по роуту

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import {Provider} from "react-redux";
-import store from "./store/store";
-import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
+import store from './store/store';
+import {BrowserRouter} from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,7 +13,9 @@ root.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App/>
+                <ScrollToTop>
+                    <App/>
+                </ScrollToTop>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>
